dab4d085326adba09171e25fd7b9e377243b47bc
Omar Alahmed <ofma99@gmail.com>
2018-03-15 17:41:48 +0200

    Delete Uber publisher core and add it to separate repo

--- modules/uber_publisher_features/uber_publisher_core/js/uber_publisher_core.scripts.js
+++
@@ -1,9 +0,0 @@
-(function ($, Drupal, drupalSettings) {
-
-	'use strict';
-
-	$(document).ready(function () {
-		// remove share buttons tiny
-		$('.sharerich-buttons').removeClass('tiny-format');
-	})
-})(jQuery, Drupal, drupalSettings);
