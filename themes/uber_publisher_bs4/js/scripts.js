38dcf12c6fe668411fd38eb7375ec2910015bca2
az <a.abuzakham@vardot.com>
2020-06-01 10:33:42 +0300

    Add new base theme based on BS4

---
+++ themes/uber_publisher_bs4/js/scripts.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Behaviors for the uber_publisher_bs4 theme.
+ */
+
+(function ($, _, Drupal, drupalSettings) {
+  'use strict';
+
+  Drupal.behaviors.uber_publisher_bs4 = {
+    attach: function (context) {
+
+      // uber_publisher_bs4 JavaScript behaviors goes here.
+    }
+  };
+
+})(window.jQuery, window._, window.Drupal, window.drupalSettings);
